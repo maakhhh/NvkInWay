cc2220d7ff06293b15e8a7e746d89b8d5a458442
Viktor Braidenbikher <v.bra@skbkontur.ru>
2025-12-01 09:52:36 +0500

    feat: добавил в ef migrations trips

---
+++ src/NvkInWay.Api/Migrations/20251130133128_AddTrips.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace NvkInWay.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddTrips : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "DriveEntityUserEntity");
+
+            migrationBuilder.DropTable(
+                name: "Drives");
+
+            migrationBuilder.CreateTable(
+                name: "Trips",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    StartPlace = table.Column<string>(type: "character varying(200)", maxLength: 200, nullable: false),
+                    EndPlace = table.Column<string>(type: "character varying(200)", maxLength: 200, nullable: false),
+                    StartTime = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: false),
+                    EndTime = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: false),
+                    SeatsCount = table.Column<int>(type: "integer", nullable: false),
+                    SeatPrice = table.Column<double>(type: "double precision", nullable: false),
+                    IsTaxi = table.Column<bool>(type: "boolean", nullable: false),
+                    Description = table.Column<string>(type: "character varying(1000)", maxLength: 1000, nullable: true),
+                    CreatorId = table.Column<long>(type: "bigint", nullable: false),
+                    CarModel = table.Column<string>(type: "character varying(200)", maxLength: 200, nullable: true),
+                    CarNumber = table.Column<string>(type: "character varying(10)", maxLength: 10, nullable: true),
+                    IsClosed = table.Column<bool>(type: "boolean", nullable: false),
+                    IsDeleted = table.Column<bool>(type: "boolean", nullable: false),
+                    IsEnded = table.Column<bool>(type: "boolean", nullable: false),
+                    CreatedAt = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: false),
+                    UpdatedAt = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Trips", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Trips_Users_CreatorId",
+                        column: x => x.CreatorId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "TripPassengers",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    TripId = table.Column<long>(type: "bigint", nullable: false),
+                    PassengerId = table.Column<long>(type: "bigint", nullable: false),
+                    IsApproved = table.Column<bool>(type: "boolean", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TripPassengers", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_TripPassengers_Trips_TripId",
+                        column: x => x.TripId,
+                        principalTable: "Trips",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_TripPassengers_Users_PassengerId",
+                        column: x => x.PassengerId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TripPassengers_PassengerId",
+                table: "TripPassengers",
+                column: "PassengerId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TripPassengers_TripId",
+                table: "TripPassengers",
+                column: "TripId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Trips_CreatorId",
+                table: "Trips",
+                column: "CreatorId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "TripPassengers");
+
+            migrationBuilder.DropTable(
+                name: "Trips");
+
+            migrationBuilder.CreateTable(
+                name: "Drives",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    DriverId = table.Column<long>(type: "bigint", nullable: true),
+                    CarModel = table.Column<string>(type: "text", nullable: true),
+                    CarNumber = table.Column<string>(type: "text", nullable: true),
+                    End = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    From = table.Column<string>(type: "character varying(200)", maxLength: 200, nullable: false),
+                    MaxPassengersCount = table.Column<int>(type: "integer", nullable: false),
+                    PriceForPlace = table.Column<int>(type: "integer", nullable: false),
+                    Start = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    TaxiEnabled = table.Column<bool>(type: "boolean", nullable: false),
+                    To = table.Column<string>(type: "character varying(200)", maxLength: 200, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Drives", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Drives_Users_DriverId",
+                        column: x => x.DriverId,
+                        principalTable: "Users",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "DriveEntityUserEntity",
+                columns: table => new
+                {
+                    DriveEntityId = table.Column<long>(type: "bigint", nullable: false),
+                    PassengersId = table.Column<long>(type: "bigint", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_DriveEntityUserEntity", x => new { x.DriveEntityId, x.PassengersId });
+                    table.ForeignKey(
+                        name: "FK_DriveEntityUserEntity_Drives_DriveEntityId",
+                        column: x => x.DriveEntityId,
+                        principalTable: "Drives",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_DriveEntityUserEntity_Users_PassengersId",
+                        column: x => x.PassengersId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_DriveEntityUserEntity_PassengersId",
+                table: "DriveEntityUserEntity",
+                column: "PassengersId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Drives_DriverId",
+                table: "Drives",
+                column: "DriverId");
+        }
+    }
+}
