548ee6d960b764fd62e7f2ec5080a67ebd725895
Ilya4rh <chernykn12ilya12@gmail.com>
2025-12-07 22:51:35 +0500

    create maui with authorization and registration pages

---
+++ app/AuthorizationPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿namespace app;
+
+public partial class AuthorizationPage : ContentPage
+{
+    public AuthorizationPage()
+    {
+        InitializeComponent();
+    }
+    
+    private async void OnSwitchToRegistrationClicked(object sender, EventArgs e)
+    {
+        await Navigation.PushAsync(new RegistrationPage());
+    }
+    
+    private void OnRegistrationClicked(object sender, EventArgs e)
+    {
+    }
+}
---
+++ app/RegistrationPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿namespace app;
+
+public partial class RegistrationPage : ContentPage
+{
+    public RegistrationPage()
+    {
+        InitializeComponent();
+    }
+
+    private async void OnSwitchToAuthorizationClicked(object? sender, EventArgs e)
+    {
+        await Navigation.PushAsync(new AuthorizationPage());
+    }
+    
+    private void OnAuthorizationClicked(object? sender, EventArgs e)
+    {
+    }
+}
